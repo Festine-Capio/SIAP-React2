964cbc3312c922a5d02f0e58ac9f872d241dd91c
Festine-Capio <festine@capioteknologi.co.id>
2021-04-07 18:23:12 +0700

    update3

---
+++ src/components/FooterContent.js
@@ -0,0 +1,11 @@
+import React from "react";
+const FooterContent = () => {
+    return(
+        <div>
+            <div className="mask">
+                <div className="row">a</div>
+            </div>
+        </div>
+    );
+};
+export default FooterContent;
